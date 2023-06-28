512c7bcef2c88b7261d097135d1b3307fc9ce10e
IdoKendo <ryuusuke@gmail.com>
2023-06-28 13:10:35 +0300

    feat: add which-key, numb, and indent-blankline plugins

--- lua/idos/packer.lua
+++ lua/idos/packer.lua
@@ -8,12 +8,39 @@ return require("packer").startup(function(use)
     }
     use {
         "nvim-treesitter/nvim-treesitter",
-        { run = ":TSUpdate" }
+        run = ":TSUpdate"
     }
     use {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup()
+        end
+    }
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup()
+        end
+    }
+    use {
+        "nacro90/numb.nvim",
+        config = function()
+            require("numb").setup()
+        end
+    }
+    use {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            vim.opt.list = true
+            vim.opt.listchars:append "space:⋅"
+            vim.opt.listchars:append "eol:↴"
+            require("indent_blankline").setup {
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            }
         end
     }
     use "nvim-treesitter/playground"
