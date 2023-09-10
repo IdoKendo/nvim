54d1a0f3e81d769d7aa4b5b74fe3756c0cdb4932
IdoKendo <ryuusuke@gmail.com>
2023-09-10 15:58:01 +0300

    feat: Add <leader>gd shortcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,8 @@ return {
     "tpope/vim-fugitive",
     keys = {
         { "<leader>gs", vim.cmd.Git, desc = "[G]it [S]how" },
-        { "<leader>ga", ':Gwrite<CR>', desc = "[G]it [A]dd" },
-        { "<leader>gb", ':Git blame<CR>', desc = "[G]it [B]lame" }
+        { "<leader>ga", '<cmd>Gwrite<CR>', desc = "[G]it [A]dd" },
+        { "<leader>gd", '<cmd>Gvdiff<CR>', desc = "[G]it [D]iff" },
+        { "<leader>gb", '<cmd>Git blame<CR>', desc = "[G]it [B]lame" }
     },
 }
