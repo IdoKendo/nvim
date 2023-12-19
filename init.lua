70006bb616c20e248e45f4a4f0c1c2c642385b17
IdoKendo <ryuusuke@gmail.com>
2023-12-19 19:07:50 +0200

    sessionx

--- init.lua
+++ init.lua
@@ -35,25 +35,25 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Remapping
-vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, { desc = "[F]ile [V]iewer"})
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, { desc = "[F]ile [V]iewer" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]], {desc = "[P]aste and keep clipboard"})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "[P]aste and keep clipboard" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/session.sh<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.config/tmux/plugins/tmux-sessionx/scripts/sessionx.sh<CR>")
 
 -- Plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
