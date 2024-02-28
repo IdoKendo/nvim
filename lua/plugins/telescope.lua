6626c3416473f555f6ae603d68ba815ccb166c10
IdoKendo <ryuusuke@gmail.com>
2024-02-28 11:12:34 +0200

    feat: change some telescope/diagnostic keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,34 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.1",
-    dependencies = { 
-        {"nvim-lua/plenary.nvim"},
+    dependencies = {
+        { "nvim-lua/plenary.nvim" },
     },
     config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "[F]ind [F]iles" })
-        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git files" })
-        vim.keymap.set('n', '<leader>fs', function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+        vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+        vim.keymap.set("n", "<leader>fs", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end, { desc = "[F]ind [S]tring" })
-        vim.keymap.set('n', '<leader>vl', builtin.resume, { desc = "[V]iew [L]ast" })
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "[V]iew [H]elp" })
-        vim.keymap.set('n', '<leader>dd', builtin.diagnostics, { desc = "[D]etailed [D]iagnostics" })
-        vim.keymap.set('n', '<leader>df', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true, desc = "[D]iagnostic [F]loat" })
+        vim.keymap.set("n", "<leader>vl", builtin.resume, { desc = "[V]iew [L]ast" })
+        vim.keymap.set("n", "<leader>vh", builtin.help_tags, { desc = "[V]iew [H]elp" })
+        vim.keymap.set("n", "<leader>dd", builtin.diagnostics, { desc = "[D]etailed [D]iagnostics" })
 
         function vim.get_visual_selection()
             vim.cmd('noau normal! "vy"')
-            local text = vim.fn.getreg('v')
+            local text = vim.fn.getreg("v")
             text = string.gsub(text, "\n", "")
             if #text > 0 then
                 return text
             else
-                return ''
+                return ""
             end
         end
 
-        vim.keymap.set('v', '<leader>fs', function()
+        vim.keymap.set("v", "<leader>fs", function()
             local text = vim.get_visual_selection()
             builtin.grep_string()
         end, { noremap = true, silent = true, desc = "[F]ind [S]tring" })
-    end
+    end,
 }
--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -1,13 +1,25 @@
+-- Buffer navigation
 vim.keymap.set("n", "}", "}zz", { desc = "Next empty line" })
 vim.keymap.set("n", "{", "{zz", { desc = "Previous empty line" })
+
+-- File navigation
 vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, { desc = "[F]ile [V]iewer" })
 
+-- Buffer manipulation
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "[P]aste and keep clipboard" })
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Diagnostic
+vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, { desc = "[D]iagnostic [F]loat" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous [D]iagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next [D]iagnostic" })
+vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "[D]iagnostic [Q]uickfix list" })
+
+-- New session script
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/scripts/session.sh<CR>")
 
+-- Stupid fun keymaps
 vim.keymap.set("n", "<leader>en", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "if [E]rr != [N]il" })
 vim.keymap.set(
     "n",
