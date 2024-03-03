57758b62b63b4043e91d2e0067fe7db621d95c5e
IdoKendo <ryuusuke@gmail.com>
2024-03-03 10:23:24 +0200

    feat: silicon

---
+++ lua/plugins/silicon.lua
@@ -0,0 +1,18 @@
+return {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    keys = {
+        { "<leader>sc", ":Silicon<CR>", mode = "v", desc = "[S]napshot [C]ode" },
+    },
+    config = function()
+        require("silicon").setup({
+            font = "FiraCode Nerd Font Mono=34;Noto Emoji=34",
+            no_line_number = true,
+            theme = "tokyonight_night",
+            window_title = function()
+                return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+            end,
+        })
+    end,
+}
