470e7fa5d7af30cdbe18b6efc37197177ac0ec77
IdoKendo <ryuusuke@gmail.com>
2024-04-21 17:48:21 +0300

    feat: and a few more git keybinds

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,10 @@ return {
     "tpope/vim-fugitive",
     keys = {
         { "<leader>gg", vim.cmd.Git, desc = "[G]it [G]it" },
-        { "<leader>ga", "<cmd>Gwrite<CR>", desc = "[G]it [A]dd" },
-        { "<leader>gd", "<cmd>Gvdiff<CR>", desc = "[G]it [D]iff" },
+        { "<leader>ga", vim.cmd.Gwrite, desc = "[G]it [A]dd" },
         { "<leader>gb", "<cmd>Git blame<CR>", desc = "[G]it [B]lame" },
+        { "<leader>gc", "<cmd>Git commit<CR>", desc = "[G]it [C]ommit" },
+        { "<leader>gd", vim.cmd.Gvdiff, desc = "[G]it [D]iff" },
+        { "<leader>gp", "<cmd>Git push<CR>", desc = "[G]it [P]ush" },
     },
 }
