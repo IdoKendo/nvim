2327aaf4a7b9190535497027765ca9141a81369a
IdoKendo <ryuusuke@gmail.com>
2024-04-21 14:47:23 +0300

    feat: add synonym.nvim

---
+++ lua/plugins/synonym.lua
@@ -0,0 +1,16 @@
+return {
+    "IdoKendo/synonym.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+        {
+            "<leader>vrs",
+            function()
+                require("synonym").find_synonym()
+            end,
+            desc = "[V]isual [R]ename [S]ynonym",
+        },
+    },
+}
