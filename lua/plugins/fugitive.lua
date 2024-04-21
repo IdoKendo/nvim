734832daa3783dfa184d1626f17b181c62e179a0
IdoKendo <ryuusuke@gmail.com>
2024-04-21 17:43:43 +0300

    feat: improve some git keybindings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,9 +1,9 @@
 return {
     "tpope/vim-fugitive",
     keys = {
-        { "<leader>gs", vim.cmd.Git, desc = "[G]it [S]how" },
-        { "<leader>ga", '<cmd>Gwrite<CR>', desc = "[G]it [A]dd" },
-        { "<leader>gd", '<cmd>Gvdiff<CR>', desc = "[G]it [D]iff" },
-        { "<leader>gb", '<cmd>Git blame<CR>', desc = "[G]it [B]lame" }
+        { "<leader>gg", vim.cmd.Git, desc = "[G]it [G]it" },
+        { "<leader>ga", "<cmd>Gwrite<CR>", desc = "[G]it [A]dd" },
+        { "<leader>gd", "<cmd>Gvdiff<CR>", desc = "[G]it [D]iff" },
+        { "<leader>gb", "<cmd>Git blame<CR>", desc = "[G]it [B]lame" },
     },
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,9 @@ return {
     lazy = false,
     keys = {
         { "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", desc = "[G]it [T]oggle blame" },
-        { "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "[G]it [P]review" },
+        { "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "[G]it [P]review hunk" },
+        { "<leader>gs", ":Gitsigns stage_hunk<CR>", desc = "[G]it [S]tage hunk", mode = { "n", "v" } },
+        { "<leader>gr", ":Gitsigns reset_hunk<CR>", desc = "[G]it [R]eset hunk", mode = { "n", "v" } },
     },
     config = function()
         require("gitsigns").setup()
