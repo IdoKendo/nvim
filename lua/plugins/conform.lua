1e83fccd19b702a9a8e1fc34a0a6eea96179b546
IdoKendo <ryuusuke@gmail.com>
2024-05-22 15:11:21 +0300

    feat: add keybinding to toggle conform for working on hostile code bases

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,16 +13,38 @@ return {
                 python = { "ruff_fix", "ruff_format", "reorder-python-imports" },
                 sql = { "sqlfmt" },
             },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
+            formatters = {
+                sqlfmt = {
+                    prepend_args = { "-l", "120" },
+                },
             },
+            format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return {
+                    lsp_fallback = true,
+                    timeout_ms = 500,
+                }
+            end,
         })
 
-        conform.formatters.sqlfmt = {
-            prepend_args = { "-l", "120" },
-        }
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = "Disable autoformat on save",
+            bang = true,
+        })
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = "Enable autoformat on save",
+        })
 
         vim.keymap.set({ "n", "v" }, "<leader>mp", function()
             conform.format({
@@ -31,5 +53,7 @@ return {
                 timeout_ms = 5000,
             })
         end, { desc = "[M]ake [P]retty" })
+        vim.keymap.set("n", "<leader>pe", "<cmd>FormatEnable<CR>", { desc = "[P]retty-on-save [E]nable" })
+        vim.keymap.set("n", "<leader>pd", "<cmd>FormatDisable<CR>", { desc = "[P]retty-on-save [D]isable" })
     end,
 }
