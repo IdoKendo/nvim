95fe0c4f77dda22d2f8756092c5e8b5dc4924c0f
IdoKendo <ryuusuke@gmail.com>
2024-07-06 11:53:49 +0300

    feat: make dadbod only load when I want, reduce startup time by 25ms

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -4,7 +4,6 @@ end
 
 return {
     "kristijanhusak/vim-dadbod-ui",
-    event = "VeryLazy",
     dependencies = {
         { "tpope/vim-dadbod", lazy = true },
         { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
