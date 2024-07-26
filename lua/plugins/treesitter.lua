f35a1a3b0f911f270dc439cf2443019e9159c858
IdoKendo <ryuusuke@gmail.com>
2024-07-26 04:30:11 -0400

    fix: add missing ensure_installed for nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
                 sync_install = false,
                 auto_install = true,
                 highlight = {
