856e2577aceaa9899fe3b244906b0e45bfbfe472
IdoKendo <ryuusuke@gmail.com>
2024-08-30 08:41:16 -0400

    chore: update whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,24 +1,27 @@
 return {
     "folke/which-key.nvim",
+    event = "VeryLazy",
+    keys = {
+        { "<leader>?", function() require("which-key").show({global = false }) end, desc = "Which Key[?]" },
+    },
     config = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 300
         local wk = require("which-key")
         wk.setup()
-        wk.register({
-            ["<leader>"] = {
-                d = { name = "[D]iagnostics" },
-                f = { name = "[F]ind" },
-                g = { name = "[G]it" },
-                h = { name = "[H]istory" },
-                l = { name = "[L]oad" },
-                m = { name = "[M]ake" },
-                o = { name = "[O]bsidian" },
-                p = { name = "[P]retty" },
-                r = { name = "[R]un" },
-                t = { name = "[T]est" },
-                v = { name = "[V]iew" },
-            },
+        wk.add({
+            { "<leader>d", group = "[D]iagnostics" },
+            { "<leader>f", group = "[F]ind" },
+            { "<leader>g", group = "[G]it" },
+            { "<leader>h", group = "[H]istory" },
+            { "<leader>l", group = "[L]oad" },
+            { "<leader>m", group = "[M]ake" },
+            { "<leader>n", group = "[N]oice" },
+            { "<leader>o", group = "[O]bsidian" },
+            { "<leader>p", group = "[P]retty" },
+            { "<leader>r", group = "[R]un" },
+            { "<leader>t", group = "[T]est" },
+            { "<leader>v", group = "[V]iew" },
         })
     end,
 }
