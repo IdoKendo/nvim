7c400ad6e4f556bee99bb5b4d79a083ea3e65299
IdoKendo <ryuusuke@gmail.com>
2024-09-07 12:38:34 +0300

    fix: render-markdown opts

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,8 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
-    opts = {},
+    opts = {
+        latex = { enabled = false },
+        acknowledge_conflicts = true,
+    },
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
