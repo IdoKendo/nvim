99a23543cddbffcb664390645a50a20a204f373f
IdoKendo <ryuusuke@gmail.com>
2024-12-15 09:17:37 +0200

    feat: fancy_macro in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,18 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "meuter/lualine-so-fancy.nvim",
+    },
     config = function()
         require("lualine").setup({
             options = { theme = "tokyonight" },
+            sections = {
+                lualine_a = {
+                    { "mode" },
+                    { "fancy_macro" },
+                },
+            },
         })
-    end
+    end,
 }
