d2ec969a815dead25a6501e0764766b1d4e80d37
IdoKendo <ryuusuke@gmail.com>
2024-12-15 09:17:45 +0200

    feat: hunk navigation

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,6 +6,8 @@ return {
         { "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "[G]it [P]review hunk" },
         { "<leader>gs", ":Gitsigns stage_hunk<CR>", desc = "[G]it [S]tage hunk", mode = { "n", "v" } },
         { "<leader>gr", ":Gitsigns reset_hunk<CR>", desc = "[G]it [R]eset hunk", mode = { "n", "v" } },
+        { "]h", ":Gitsigns next_hunk<CR>", desc = "Next [H]unk", mode = "n" },
+        { "[h", ":Gitsigns prev_hunk<CR>", desc = "Previous [H]unk", mode = "n" },
     },
     config = function()
         require("gitsigns").setup()
