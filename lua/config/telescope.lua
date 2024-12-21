46a07cfcf23ad3c62c5641f7dfdae828e5884d5f
IdoKendo <ryuusuke@gmail.com>
2024-12-21 21:25:40 +0200

    feat: leveling up my <leader>fs

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,11 +1,61 @@
 local builtin = require("telescope.builtin")
+local config = require("telescope.config")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local pickers = require("telescope.pickers")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
 vim.keymap.set("n", "<leader>fg", function()
     builtin.git_files({ recurse_submodules = true })
 end, { desc = "[F]ind [G]it files" })
 vim.keymap.set("n", "<leader>fs", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    local opts = {}
+    opts.cwd = opts.cwd or vim.uv.cwd()
+
+    local finder = finders.new_async_job({
+        command_generator = function(prompt)
+            if not prompt or prompt == "" then
+                return nil
+            end
+
+            local parts = vim.split(prompt, "  ")
+            local args = { "rg" }
+            if parts[1] then
+                table.insert(args, "-e")
+                table.insert(args, parts[1])
+            end
+
+            if parts[2] then
+                table.insert(args, "-g")
+                table.insert(args, parts[2])
+            end
+
+            ---@diagnostic disable-next-line: deprecated
+            return vim.tbl_flatten({
+                args,
+                {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                },
+            })
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(opts),
+        cwd = opts.cwd,
+    })
+
+    pickers
+        .new(opts, {
+            debounce = 100,
+            prompt_title = "Find String",
+            finder = finder,
+            previewer = config.values.grep_previewer(opts),
+            sorter = require("telescope.sorters").empty(),
+        })
+        :find()
 end, { desc = "[F]ind [S]tring" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind existing [B]uffers" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
