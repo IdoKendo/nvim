32c4647cf3be389a8e5db87594564cb03249f9f4
IdoKendo <ryuusuke@gmail.com>
2025-03-05 11:13:17 +0200

    chore!: upgrade all plugins

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,6 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
         latex = { enabled = false },
-        acknowledge_conflicts = true,
     },
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
