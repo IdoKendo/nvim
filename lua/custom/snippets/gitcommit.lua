705ea6aaf589a444a4b38e4592f4d43aca735654
IdoKendo <ryuusuke@gmail.com>
2025-04-06 14:34:06 +0300

    feat!: blink.cmp

--- lua/custom/snippets/gitcommit.lua
+++
@@ -1,9 +0,0 @@
-local luasnip = require("luasnip")
-
-local s = luasnip.snippet
-local t = luasnip.text_node
-local i = luasnip.insert_node
-
-luasnip.add_snippets("gitcommit", {
-    s("conventional commit", { i(1, "type"), t(": "), i(2, "description") }),
-})
---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,63 @@
+return {
+    {
+        "saghen/blink.compat",
+        version = "*",
+        lazy = true,
+        opts = {},
+    },
+    {
+        "saghen/blink.cmp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            "rcarriga/cmp-dap",
+        },
+        version = "1.*",
+        opts = {
+            keymap = {
+                preset = "enter",
+                ["<C-j>"] = { "select_next" },
+                ["<C-k>"] = { "select_prev" },
+            },
+
+            enabled = function()
+                local cmp_dap = require("cmp_dap")
+                return vim.bo.buftype ~= "prompt" or cmp_dap.is_dap_buffer()
+            end,
+
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+
+            completion = {
+                documentation = { auto_show = true },
+                menu = {
+                    border = "rounded",
+                    draw = {
+                        columns = {
+                            { "label", "label_description", gap = 1 },
+                            { "kind_icon", "kind" },
+                        },
+                    },
+                },
+            },
+
+            signature = {
+                enabled = true,
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+                per_filetype = {
+                    sql = { "snippets", "dadbod" },
+                },
+                providers = {
+                    dadbod = { name = "dadbod", module = "vim_dadbod_completion.blink" },
+                    dap = { name = "dap", module = "blink.compat.source" },
+                },
+            },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        },
+        opts_extend = { "sources.default" },
+    },
+}
