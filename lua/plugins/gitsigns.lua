adf0fb9893fa028b6e8337f5ff167bd63312d962
IdoKendo <ryuusuke@gmail.com>
2025-04-30 10:01:08 +0300

    feat: goodbye treewalker hello <leader>gS

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,18 @@ return {
         { "<leader>gr", ":Gitsigns reset_hunk<CR>", desc = "[G]it [R]eset hunk", mode = { "n", "v" } },
         { "]h", ":Gitsigns next_hunk<CR>", desc = "Next [H]unk", mode = "n" },
         { "[h", ":Gitsigns prev_hunk<CR>", desc = "Previous [H]unk", mode = "n" },
+        {
+            "<leader>gS",
+            function()
+                local sha = vim.fn.input("Commit SHA: ")
+                if sha and sha ~= "" then
+                    local command = ":Gitsigns show " .. sha
+                    vim.cmd(command)
+                end
+            end,
+            desc = "[G]it [S]how commit",
+            mode = "n",
+        },
     },
     config = function()
         require("gitsigns").setup()
