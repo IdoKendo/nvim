71d0ce56c828f5deff1f04e5594e1a85ffb17508
IdoKendo <ryuusuke@gmail.com>
2025-07-09 12:10:42 +0300

    feat: nvim --noplugin colorscheme

--- lua/settings/color_scheme.lua
+++ lua/settings/color_scheme.lua
@@ -1,1 +1,15 @@
-vim.cmd.colorscheme("tokyonight")
+local function try_colorschemes(...)
+    for _, cs in ipairs({ ... }) do
+        local ok = pcall(vim.cmd.colorscheme, cs)
+        if ok then
+            return
+        end
+    end
+    vim.notify("No preferred colorschemes found. Using 'unokai'.", vim.log.levels.WARN)
+    vim.cmd.colorscheme("unokai")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+
+try_colorschemes("tokyonight")
