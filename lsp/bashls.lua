eff2919d2930970c46fec07833b46bd51c35ac6e
IdoKendo <ryuusuke@gmail.com>
2025-11-12 10:19:33 +0200

    fix: bash langauge server autodetection

--- lsp/bashls.lua
+++ lsp/bashls.lua
@@ -1,5 +1,5 @@
 return {
     cmd = { "bash-language-server", "start" },
-    filetypes = { "bash", "sh" },
-    root_markers = { ".git" },
+    filetypes = { "bash", "sh", "zsh" },
+    root_markers = { ".git", ".gitignore" },
 }
