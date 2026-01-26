ce264d6c2d1dc86d14d040846c0c0f2a4e346492
IdoKendo <ryuusuke@gmail.com>
2026-01-26 16:11:52 +0200

    fix(bashls): lint with zsh in mind

--- lsp/bashls.lua
+++ lsp/bashls.lua
@@ -2,4 +2,9 @@ return {
     cmd = { "bash-language-server", "start" },
     filetypes = { "bash", "sh", "zsh" },
     root_markers = { ".git", ".gitignore" },
+    settings = {
+        bashIde = {
+            shellcheckArguments = "-s zsh",
+        },
+    },
 }
