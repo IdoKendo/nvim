cbf1470d574891f39b9b04ae4485b44a4d268e34
IdoKendo <ryuusuke@gmail.com>
2026-02-11 17:16:40 +0200

    feat: diffs.lua

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,7 +5,7 @@ return {
         { "<leader>ga", vim.cmd.Gwrite, desc = "[G]it [A]dd" },
         { "<leader>gb", "<cmd>Git blame<CR>", desc = "[G]it [B]lame" },
         { "<leader>gc", "<cmd>Git commit<CR>", desc = "[G]it [C]ommit" },
-        { "<leader>gd", vim.cmd.Gvdiff, desc = "[G]it [D]iff" },
+        { "<leader>gd", ":Gdiffsplit!<CR>", desc = "[G]it [D]iff" },
         { "<leader>gp", "<cmd>Git push<CR>", desc = "[G]it [P]ush" },
         { "<leader>gl", "<cmd>Git pull<CR>", desc = "[G]it pul[L]" },
     },
