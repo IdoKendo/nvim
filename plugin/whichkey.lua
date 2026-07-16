40d938b0e23f28923e08b5e3410f669e8c0aa2ec
IdoKendo <ryuusuke@gmail.com>
2026-07-16 13:05:07 +0300

    feat(whichkey): change to modern preset

--- plugin/whichkey.lua
+++ plugin/whichkey.lua
@@ -6,19 +6,20 @@ end, { desc = "Which Key[?]" })
 
 local wk = require("which-key")
 
-wk.setup()
-
-wk.add({
-    { "<leader>d", group = "[D]iagnostics" },
-    { "<leader>f", group = "[F]ind" },
-    { "<leader>g", group = "[G]it" },
-    { "<leader>h", group = "[H]istory" },
-    { "<leader>l", group = "[L]oad" },
-    { "<leader>m", group = "[M]ake" },
-    { "<leader>n", group = "[N]oice" },
-    { "<leader>o", group = "[O]bsidian" },
-    { "<leader>p", group = "[P]retty" },
-    { "<leader>r", group = "[R]un" },
-    { "<leader>t", group = "[T]oggle" },
-    { "<leader>w", group = "[W]alk" },
+wk.setup({
+    preset = "modern",
+    spec = {
+        { "<leader>d", group = "[D]iagnostics" },
+        { "<leader>f", group = "[F]ind" },
+        { "<leader>g", group = "[G]it" },
+        { "<leader>h", group = "[H]istory" },
+        { "<leader>l", group = "[L]oad" },
+        { "<leader>m", group = "[M]ake" },
+        { "<leader>n", group = "[N]oice" },
+        { "<leader>o", group = "[O]bsidian" },
+        { "<leader>p", group = "[P]retty" },
+        { "<leader>r", group = "[R]un" },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>w", group = "[W]alk" },
+    },
 })
